b7ecab7997e5ef0fac063827400cafb2b83f0e6a
artem111-oss <a.vasiliev86i@gmail.com>
2026-07-20 07:49:58 +0300

    fix: allow telegram login widget in csp

--- app/Http/Middleware/SecurityHeaders.php
+++ app/Http/Middleware/SecurityHeaders.php
@@ -20,12 +20,12 @@ class SecurityHeaders
         // Content-Security-Policy (CSP) - защита от XSS
         $response->headers->set('Content-Security-Policy', 
             "default-src 'self'; " .
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mc.yandex.ru https://yandex.ru https://code.jivosite.com https://*.jivo.ru https://*.jivosite.com; " .
+            "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://mc.yandex.ru https://yandex.ru https://code.jivosite.com https://telegram.org https://*.jivo.ru https://*.jivosite.com; " .
             "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " .
             "font-src 'self' https://fonts.gstatic.com; " .
             "img-src 'self' data: https: http:; " .
-            "connect-src 'self' https://mc.yandex.ru https://*.jivo.ru https://*.jivosite.com; " .
-            "frame-src https://money.yandex.ru https://yoomoney.ru https://secure.platima.net; " .
+            "connect-src 'self' https://mc.yandex.ru https://telegram.org https://*.jivo.ru https://*.jivosite.com; " .
+            "frame-src https://money.yandex.ru https://yoomoney.ru https://secure.platima.net https://oauth.telegram.org; " .
             "object-src 'none'; " .
             "base-uri 'self';"
         );
